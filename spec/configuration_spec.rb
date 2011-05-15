60381a4f7631ff63f6358305631866832a13d85d
Uģis Ozols <ugis.ozolss@gmail.com>
2011-05-16 00:11:05 +0300

    Seperate specs.

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -1,5 +1,47 @@
 require "spec_helper"
 
 describe FiltersSpam::Configuration do
-  pending
+
+  let(:config) { FiltersSpam::Configuration.new }
+
+  it "configures message field" do
+    config.message_field = :rspec
+    config.message_field.should == :rspec
+  end
+
+  it "configures email field" do
+    config.email_field = :rspec
+    config.email_field.should == :rspec
+  end
+
+  it "configures author field" do
+    config.author_field = :rspec
+    config.author_field.should == :rspec
+  end
+
+  it "configures other fields" do
+    config.other_fields = :rspec
+    config.other_fields.should include(:rspec)
+  end
+
+  it "adds extra spam words to spam words list" do
+    config.extra_spam_words = "rspec"
+    config.spam_words.should include("rspec")
+  end
+
+  it "configures recpatcha usage" do
+    config.use_recaptcha = true
+    config.use_recaptcha.should == true
+  end
+
+  it "configures recaptcha public key" do
+    config.recaptcha_public_key = "777"
+    config.recaptcha_public_key.should == "777"
+  end
+
+  it "configures recaptcha private key" do
+    config.recaptcha_private_key = "777"
+    config.recaptcha_private_key.should == "777"
+  end
+
 end
